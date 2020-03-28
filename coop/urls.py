44b1cfada9745ae6d31d0163f112a971db4f5bb3
jtkim03 <jtk2rw@virginia.edu>
2020-03-28 18:04:01 -0400

    Basic profile

--- coop/urls.py
+++ coop/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
     path('settings/', views.settings, name='settings'),
     path('profile/', views.profile, name='profile'),
-]
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webapp/urls.py
+++ webapp/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('',TemplateView.as_view(template_name = 'coop/index.html'), name='coop-home'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'coop/index.html'), name = 'logout'),
-    #path('', include('coop.urls')),
+    path('', include('coop.urls')),
     # path('coop/', include('coop.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
