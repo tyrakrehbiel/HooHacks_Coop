6ee37f05b0f7f8b1f0a43bfcc4646fc18dc5a18c
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-03-28 13:26:25 -0400

    Views have been edited, and html pages have been created for the essential views

--- coop/urls.py
+++ coop/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload'),
+    path('settings/', views.settings, name='settings'),
+    path('profile/', views.profile, name='profile'),
 ]
--- coop/views.py
+++ coop/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    # here we display the feed
     return HttpResponse("This is the home page")
+
+def profile(request):
+    return HttpResponse("This is the profile page")
+
+def feed(request):
+    return HttpResponse("This is the feed page")
+
+def upload(request):
+    return HttpResponse("This is the page to upload notes documents")
+
+def settings(request):
+    return HttpResponse("This is the settings page")
