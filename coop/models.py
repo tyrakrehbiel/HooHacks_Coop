3cb52ffdd4b83bd9431589c844b89d9cb17b266b
jtkim03 <jtk2rw@virginia.edu>
2020-03-29 15:29:43 -0400

    note author

--- coop/models.py
+++ coop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Notes(models.Model):
@@ -6,7 +7,7 @@ class Notes(models.Model):
     topic = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     noteFile = models.FileField(upload_to = 'notes/')
-
+    author = models.ForeignKey(User,on_delete = models.CASCADE)
     def __str__(self):
         return "Course Name: " + self.courseName + " Topic: " + self.topic
 
