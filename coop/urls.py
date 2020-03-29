3ee9c815f494b158785e26d2a41ad00a61b21b6a
Won Young Choi <wc7qpe@virginia.edu>
2020-03-29 00:33:58 -0400

    Organized upload URLs

--- coop/urls.py
+++ coop/urls.py
@@ -7,12 +7,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/', views.upload, name='upload'),
     path('settings/', views.settings, name='settings'),
     path('profile/', views.profile, name='profile'),
 
     path('notes/', views.notes_list, name='notes_list'),
-    path('notes/upload_note/', views.upload_note, name='upload_note'),
+    path('notes/upload/', views.upload_note, name='upload_note'),
     path('notes/<int:pk>/', views.delete_note, name='delete_note'),
 
 
